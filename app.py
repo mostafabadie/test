a624b667423ccb1eeaca9b20a9374962ae39d5d5
mostafabadie <67068960+mostafabadie@users.noreply.github.com>
2026-04-27 00:21:15 +0300

    .

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
 BASE_DIR = os.path.dirname(__file__)
-font_path = os.path.join(BASE_DIR, "fonts", "arial.ttf")
+font_path = os.path.join(BASE_DIR, "fonts", "NotoSansArabic-Bold.ttf")
 
 pdfmetrics.registerFont(TTFont('Arabic', font_path))
 import io
